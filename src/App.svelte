65b823d4201d366de936bf0f5acf954f83da98b1
derryleng <derry.leng@gmail.com>
2023-09-20 11:25:58 +0100

    Added WIP ImportExport

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,9 @@
 <script>
   import Bookmarks from './lib/Bookmarks.svelte'
+  import ImportExport from './lib/ImportExport.svelte'
 </script>
 
 <main>
   <Bookmarks />
+  <ImportExport />
 </main>
---
+++ src/lib/ImportExport.svelte
@@ -0,0 +1,60 @@
+<script>
+  function importBookmarks() {
+    const inputElement = document.createElement('input');
+    inputElement.type = 'file';
+    inputElement.accept = '.html, .json';
+    inputElement.click();
+
+    inputElement.addEventListener('change', (event) => {
+      const selectedFile = event.target.files[0];
+
+      if (!selectedFile) {
+        alert('No file selected.');
+        return;
+      }
+
+      const fileType = selectedFile.type;
+
+      if (fileType === 'text/html') {
+        readHTMLFile(selectedFile);
+      } else if (fileType === 'application/json') {
+        readJSONFile(selectedFile);
+      } else {
+        alert('Unsupported file type. Please select an HTML file (for Chrome bookmarks) or JSON file (for Firefox bookmarks).');
+      }
+    });
+  }
+
+  function readHTMLFile(file) {
+    const reader = new FileReader();
+
+    reader.onload = function (event) {
+      const htmlContent = event.target.result;
+      // TODO: Process the HTML content here
+      console.log('HTML content:', htmlContent);
+    };
+
+    reader.readAsText(file);
+  }
+
+  function readJSONFile(file) {
+    const reader = new FileReader();
+
+    reader.onload = function (event) {
+      const jsonContent = JSON.parse(event.target.result);
+      // TODO: Process the JSON content here
+      console.log('JSON content:', jsonContent);
+    };
+
+    reader.readAsText(file);
+  }
+
+  function exportBookmarks() {
+    // TODO
+  }
+</script>
+
+<div class="card">
+  <button on:click={importBookmarks}>Import</button>
+  <button on:click={exportBookmarks}>Export</button>
+</div>
